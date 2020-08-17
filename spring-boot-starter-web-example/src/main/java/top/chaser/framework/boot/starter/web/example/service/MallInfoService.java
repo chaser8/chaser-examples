b9d91c1cf8011408481f32a889c3d34930d77206
chaser8 <chaser8@163.com>
2020-08-17 17:45:37 +0800

    add spring-boot-starter-web-example,swagger

---
+++ spring-boot-starter-web-example/src/main/java/top/chaser/framework/boot/starter/web/example/service/MallInfoService.java
@@ -0,0 +1,10 @@
+package top.chaser.framework.boot.starter.web.example.service;
+
+import org.springframework.stereotype.Service;
+import top.chaser.framework.boot.starter.tkmybatis.service.TkServiceImpl;
+import top.chaser.framework.boot.starter.web.example.model.MallInfo;
+
+@Service
+public class MallInfoService extends TkServiceImpl<MallInfo> {
+
+}
